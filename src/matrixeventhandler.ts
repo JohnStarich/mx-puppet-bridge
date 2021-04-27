bc47584b07baaa1b37077eeef82fd88ae3375682
John Starich <johnstarich@johnstarich.com>
2021-04-26 20:59:43 -0500

    Support messages from encrypted rooms via pantalaimon

--- src/matrixeventhandler.ts
+++ src/matrixeventhandler.ts
@@ -199,7 +199,7 @@ export class MatrixEventHandler {
 			return;
 		}
 		// we handle stickers and reactions as message events
-		if (["m.reaction", "m.sticker", "m.room.message"].includes(event.type)) {
+		if (["m.reaction", "m.sticker", "m.room.message", "m.room.encrypted"].includes(event.type)) {
 			const evt = new MessageEvent<MessageEventContent>(event.raw);
 			await this.handleMessageEvent(roomId, evt);
 			return;
